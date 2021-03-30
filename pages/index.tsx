53428df0a3207812b979534c70a218cd1843bb16
ThisSilenceIsMine <49694311+ThisSilenceIsMine@users.noreply.github.com>
2021-03-30 19:54:39 +0300

    Bit a trouble

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,7 @@
-<<<<<<< HEAD
 import { signIn, signOut, useSession } from 'next-auth/client'
 
 const Index = () => {
     const [session, loading] = useSession()
-=======
-import ToDoList from "@components/ToDoList";
-import Link from "next/link";
-import dbConnect from "../utils/dbConnect";
-
-const Index = () => (
-    <>
-        <div className="container mx-auto">
-            <ToDoList />
-        </div>
-    </>
-);
->>>>>>> Fixes, basic todolist
 
     if (loading) {
         return <p>Loading..</p>
